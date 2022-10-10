4b1f02f2666ed819702cb851db31b8044ed3a21f
Sriramkarthikakula <92425111+Sriramkarthikakula@users.noreply.github.com>
2022-10-10 20:14:33 +0530

    Create down-left inclined right angled triangle.py

---
+++ down-left inclined right angled triangle.py
@@ -0,0 +1,15 @@
+"""this code prints the below pattern
+if x = 5 the pattern is:-
+ *****
+  ****
+   ***
+    **
+     *
+"""
+#the code is:-
+
+x = int(input("Enter: "))
+for i in range(x):
+    print(" "*i,end=" ")
+    print("*"*(x-i))
+
