23cd782a8cd7ce1ec1a6bfe42abf34df9946ff80
Sriramkarthikakula <92425111+Sriramkarthikakula@users.noreply.github.com>
2022-10-11 19:52:26 +0530

    Create left inclined equilateral triangle.py

---
+++ left inclined equilateral triangle.py
@@ -0,0 +1,26 @@
+"""this code prints the below pattern
+if x = 5 the pattern is:-
+      *
+     **
+    ***
+   ****
+  *****
+ ******
+  *****
+   ****
+    ***
+     **
+      *
+"""
+#the code is:-
+
+
+n = int(input("Enter: "))
+for i in range(1,2*n+1):
+    if i<=n:
+        print(" "*(n-i),end=" ")
+        print("*"*i)
+    if i>n:
+        print(" "*(n-((2*n)-i)),end=" ")
+        print("*"*((2*n)-i))
+    
