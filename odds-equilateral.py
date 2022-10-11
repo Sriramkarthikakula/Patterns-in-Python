6d3a51d12a089639577750dc662955ce7028f557
Sriramkarthikakula <92425111+Sriramkarthikakula@users.noreply.github.com>
2022-10-11 20:12:01 +0530

    Create odds-equilateral.py

---
+++ odds-equilateral.py
@@ -0,0 +1,16 @@
+"""this code prints the below pattern
+if n = 5 the pattern is:-
+     *
+    ***
+   *****
+  *******
+ *********
+"""
+#the code is:-
+
+
+n = int(input("enter: "))
+for i in range(1,n+1):
+    if i<=n:
+        print(" "*(n-i),end=" ")
+        print("*"*((2*i)-1))
